ecdbd8567e6d26a3653d66f8fc61d98f1379be7e
Sahar Pk <sahar.pk88@gmail.com>
2020-09-25 09:07:42 +0200

    Added the service layer.

--- src/main/java/com/spk/web/myfirstws/ui/controller/UserController.java
+++ src/main/java/com/spk/web/myfirstws/ui/controller/UserController.java
@@ -1,21 +1,33 @@
 package com.spk.web.myfirstws.ui.controller;
 
+import com.spk.web.myfirstws.service.UserService;
+import com.spk.web.myfirstws.shared.dto.UserDto;
 import com.spk.web.myfirstws.ui.model.request.UserDetailsRequest;
 import com.spk.web.myfirstws.ui.model.response.UserRest;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping
-    public String getUser(){
+    public String getUser() {
         return "first web app get user was called.";
     }
 
     @PostMapping
-    public UserRest createUser(@RequestBody UserDetailsRequest userDetails){
-        return null;
+    public UserRest createUser(@RequestBody UserDetailsRequest userDetails) {
+        UserRest returnValue = new UserRest();
+        UserDto userDto = new UserDto();
+        BeanUtils.copyProperties(userDetails, userDto);
+        UserDto createdUser = userService.createUser(userDto);
+        BeanUtils.copyProperties(createdUser, returnValue);
+        return returnValue;
     }
 
     @PutMapping
