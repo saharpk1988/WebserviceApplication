1829f4e11b58dfd97c3ab67bc39fce93aadf596b
Sahar Pk <sahar.pk88@gmail.com>
2020-09-25 09:09:42 +0200

    Added the service layer implementation.

---
+++ src/main/java/com/spk/web/myfirstws/service/UserService.java
@@ -0,0 +1,7 @@
+package com.spk.web.myfirstws.service;
+
+import com.spk.web.myfirstws.shared.dto.UserDto;
+
+public interface UserService {
+    UserDto createUser(UserDto userDto);
+}
---
+++ src/main/java/com/spk/web/myfirstws/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.spk.web.myfirstws.service.impl;
+
+import com.spk.web.myfirstws.service.UserService;
+import com.spk.web.myfirstws.shared.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        return null;
+    }
+}
