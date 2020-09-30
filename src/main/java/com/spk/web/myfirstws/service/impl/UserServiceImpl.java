19c7351553db1182bc0f5252136fee14ecd94a68
Sahar Pk <sahar.pk88@gmail.com>
2020-09-30 10:00:20 +0200

    Implementing UserLoginRequest class and loadUserByUsername method used for the sign in.

--- src/main/java/com/spk/web/myfirstws/service/impl/UserServiceImpl.java
+++ src/main/java/com/spk/web/myfirstws/service/impl/UserServiceImpl.java
@@ -7,10 +7,13 @@ import com.spk.web.myfirstws.shared.Utils;
 import com.spk.web.myfirstws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -42,7 +45,9 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findUserByEmail(email);
+        if (userEntity == null) throw new UsernameNotFoundException(email);
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
     }
 }
