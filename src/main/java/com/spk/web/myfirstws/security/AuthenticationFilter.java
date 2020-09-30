3950d7108596129ca704e028622d9ff5162200c8
Sahar Pk <sahar.pk88@gmail.com>
2020-09-30 11:11:17 +0200

    Adding SecurityConstants and Implementing AuthenticationFilter.

---
+++ src/main/java/com/spk/web/myfirstws/security/AuthenticationFilter.java
@@ -0,0 +1,58 @@
+package com.spk.web.myfirstws.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spk.web.myfirstws.ui.model.request.UserLoginRequest;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            UserLoginRequest creds = new ObjectMapper()
+                    .readValue(request.getInputStream(), UserLoginRequest.class);
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getEmail(),
+                            creds.getPassword(),
+                            new ArrayList<>()
+                    )
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String userName = ((User) authResult.getPrincipal()).getUsername();
+
+        String token = Jwts.builder()
+                .setSubject(userName)
+                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET)
+                .compact();
+
+        response.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+    }
+}
---
+++ src/main/java/com/spk/web/myfirstws/security/SecurityConstants.java
@@ -0,0 +1,14 @@
+package com.spk.web.myfirstws.security;
+
+public class SecurityConstants {
+    //Token gonna be valid for 10 days
+    public static final long EXPIRATION_TIME = 864000000; //This value in ms.
+
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+
+    public static final String SIGN_UP_URL = "/users";
+
+    //This will be used in the encryption of value of the access token
+    public static final String TOKEN_SECRET = "jf9i4jgu83nfl0";
+}
