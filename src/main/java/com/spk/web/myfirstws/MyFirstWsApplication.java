9b243f8f2e5c36b6e75a985be008637a5eeaa33e
Sahar Pk <sahar.pk88@gmail.com>
2020-10-06 12:41:59 +0200

    Changed the project to create a war file to deploy to the AWS EC2.

--- src/main/java/com/spk/web/myfirstws/MyFirstWsApplication.java
+++ src/main/java/com/spk/web/myfirstws/MyFirstWsApplication.java
@@ -3,11 +3,18 @@ package com.spk.web.myfirstws;
 import com.spk.web.myfirstws.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class MyFirstWsApplication {
+public class MyFirstWsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MyFirstWsApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyFirstWsApplication.class, args);
