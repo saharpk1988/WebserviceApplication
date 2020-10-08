80cd3d3c8cdb8deec790bfde3e8563b3e897e843
Sahar Pk <sahar.pk88@gmail.com>
2020-10-08 22:43:26 +0200

    Implementing getAddresses method business logic and creating AddressRepository class.

---
+++ src/main/java/com/spk/web/myfirstws/io/repositories/AddressRepository.java
@@ -0,0 +1,13 @@
+package com.spk.web.myfirstws.io.repositories;
+
+import com.spk.web.myfirstws.io.entity.AddressEntity;
+import com.spk.web.myfirstws.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AddressRepository extends CrudRepository<AddressEntity, Long> {
+    List<AddressEntity> findAllByUserDetails(UserEntity userEntity);
+}
--- src/main/java/com/spk/web/myfirstws/service/impl/AddressServiceImpl.java
+++ src/main/java/com/spk/web/myfirstws/service/impl/AddressServiceImpl.java
@@ -1,15 +1,39 @@
 package com.spk.web.myfirstws.service.impl;
 
+import com.spk.web.myfirstws.io.entity.AddressEntity;
+import com.spk.web.myfirstws.io.entity.UserEntity;
+import com.spk.web.myfirstws.io.repositories.AddressRepository;
+import com.spk.web.myfirstws.io.repositories.UserRepository;
 import com.spk.web.myfirstws.service.AddressService;
 import com.spk.web.myfirstws.shared.dto.AddressDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class AddressServiceImpl implements AddressService {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    AddressRepository addressRepository;
+
     @Override
     public List<AddressDto> getAddresses(String userId) {
-        return null;
+        List<AddressDto> returnValue = new ArrayList<>();
+
+        UserEntity userEntity = userRepository.findByUserId(userId);
+        if (userEntity == null) return returnValue;
+
+        ModelMapper modelMapper = new ModelMapper();
+        List<AddressEntity> addresses = addressRepository.findAllByUserDetails(userEntity);
+        for (AddressEntity addressEntity : addresses) {
+            returnValue.add(modelMapper.map(addressEntity, AddressDto.class));
+        }
+
+        return returnValue;
+
     }
 }
