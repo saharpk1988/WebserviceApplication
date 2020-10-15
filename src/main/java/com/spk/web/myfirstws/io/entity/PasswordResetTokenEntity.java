328102fe74cfd2d383af57084a6039e50b15a432
Sahar Pk <sahar.pk88@gmail.com>
2020-10-15 14:45:56 +0200

    Creating PasswordResetTokenEntity class & PasswordResetTokenRepository interface.

--- src/main/java/com/spk/web/myfirstws/io/entity/PasswordResetTokenEntity.java
+++ src/main/java/com/spk/web/myfirstws/io/entity/PasswordResetTokenEntity.java
@@ -1,8 +1,30 @@
 package com.spk.web.myfirstws.io.entity;
 
-public class PasswordResetTokenEntity {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity(name = "password_reset_tokens")
+public class PasswordResetTokenEntity implements Serializable {
+
+    private static final long serialVersionUID = 7863862991269895321L;
+    @Id
+    @GeneratedValue
+    private long id;
+
     private String token;
-    private UserEntity userDetails;
+
+    @OneToOne // One token can be only associated to one user
+    @JoinColumn(name = "users_id")
+    private UserEntity userDetails; // foreign key of password_reset_tokens table
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getToken() {
         return token;
