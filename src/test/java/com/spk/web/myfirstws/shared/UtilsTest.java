041a16ad2bdd99ae9cb4accb15cedeec0e554ec2
Sahar Pk <sahar.pk88@gmail.com>
2020-11-09 13:07:27 +0100

    Testing the generateUserId method in the Utils class.

---
+++ src/test/java/com/spk/web/myfirstws/shared/UtilsTest.java
@@ -0,0 +1,45 @@
+package com.spk.web.myfirstws.shared;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class UtilsTest {
+
+    @Autowired
+    Utils utils;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    final void testGenerateUserId() {
+        String userId = utils.generateUserId(30);
+        String userId2 = utils.generateUserId(30);
+
+        assertNotNull(userId);
+        assertNotNull(userId2);
+
+        assertTrue(userId.length() == 30);
+        assertTrue(!userId.equalsIgnoreCase(userId2));
+
+    }
+
+    @Test
+    @Disabled
+    final void testHasTokenExpired() {
+        fail("Not yet implemented");
+    }
+
+
+}
